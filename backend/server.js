d4287577dfefbf8dd275b7cd0e06decfeb45ce42
riteshkumar34 <riteshandshan@gmail.com>
2025-11-21 15:46:20 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
-import notesRoutes from "./routes/notesRoutes.js";
-import { connectDB } from "./config/db.js";
+import notesRoutes from "./src/routes/notesRoutes.js";
+import { connectDB } from "./src/config/db.js";
 import dotenv from "dotenv";
 import cors from "cors"
 
