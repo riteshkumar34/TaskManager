a16df47c0cda52748220b94410d62623d2020039
riteshkumar34 <riteshandshan@gmail.com>
2025-11-22 11:06:28 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,12 @@ const PORT = process.env.PORT || 5001;
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174","https://task-manager-git-main-riteshkumar34s-projects.vercel.app/"], 
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://task-manager-git-main-riteshkumar34s-projects.vercel.app",
+    "https://task-manager-tau-mauve.vercel.app"
+  ], 
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
