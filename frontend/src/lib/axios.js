8308725b85a147f9dfd666876ec1bfa41bb64cb8
riteshkumar34 <riteshandshan@gmail.com>
2025-11-22 10:54:25 +0530

    commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,14 @@
-import axios from "axios"
+import axios from "axios";
 
-const api=axios.create({
-    baseURL:"http://localhost:5001/api",
+const BASE_URLS = [
+    "http://localhost:5001/api",
+    "https://taskmanager-4-x5eq.onrender.com/api"
+];
+
+const api = axios.create({
+    baseURL: process.env.NODE_ENV === "development"
+        ? BASE_URLS[0]
+        : BASE_URLS[1],
 });
-
 
 export default api;
